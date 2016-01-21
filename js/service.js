babc2a10707ae02172eec4656d8ea71138ad2fcd
chaoen <jason831128@gmail.com>
2016-01-21 11:43:26 +0800

    add edit

--- js/service.js
+++ js/service.js
@@ -1,10 +1,10 @@
-angular.module('Service', [])
+angular.module('App', [])
 
 .service('UserService', ['$scope', '$http', 'http://localhost:8000', function($scope, $http, $URL) {
 	$scope.getUser = function() {
-		$http({method: 'GET', url: $URL}).
+		$http({method: 'GET', url: 'http://localhost:8000/users'}).
 			then(function(response) {
-				console.log(response.data);
+				console.log(response);
 			})
 	}
 }])
--- js/user.js
+++ js/user.js
@@ -1,5 +1,4 @@
-angular.module('usermodule', ['ui.router', 'Routing', 'ngMaterial', 'ngMessages', 'Service'])
-
+angular.module('usermodule', ['ui.router', 'Routing', 'ngMaterial', 'ngMessages'])
 	.config(function($stateProvider, $urlRouterProvider, routerProvider) {
 		$stateProvider
 			.state('user', {
@@ -16,44 +15,122 @@ angular.module('usermodule', ['ui.router', 'Routing', 'ngMaterial', 'ngMessages'
 				controllerAs: 'list',
 				templateUrl: 'templates/user/list.html'
 			})
+
+			.state('user.edit', {
+				url: '',
+				parent: 'user',
+				controller: 'UserEditController',
+				controllerAs: 'edit',
+				templateUrl: 'templates/dialog/edit.html'
+			})
 	})
 
 	
+	.controller('UserListController',['$scope',
+		'$mdDialog',
+		'$http',
+		function($scope, $mdDialog, $http) {
+			// $scope.user = null;
+			$scope.URL = 'http://localhost:8000';
+			$http({
+				method:'GET', url:$scope.URL+'/users'}).
+				then(function(response){
+					$scope.users=response.data;
+				})
 
-	.controller('UserListController',['$scope', '$mdDialog', function($scope, $mdDialog) {
-		
+			$scope.editDialog = function($event, userId) {
+				$mdDialog.show({
+					controller: function ($scope, $http, $mdDialog) {
+						$http({
+							method:'GET', url:'http://localhost:8000/'+userId}).
+							then(function(response){
+								$scope.user=response.data;
+							})
+						$scope.cancel = function() {
+							$mdDialog.cancel();
+						}
 
-		$scope.users = [
-            {"name":"jason","phone":"09123456789","address":"123123"},
-            {"name":"jack","phone":"0123123123","address":"12312322"},
-            {"name":"ckck","phone":"123131","address":"song"}
-        ]
-
-
-        $scope.create = function($event) {
-        	$mdDialog.show({
-		        controller: 'UserCreateController',
-		        controllerAs: 'create',
-		        templateUrl: 'templates/dialog/dialog.tmpl.html',
-		        parent: angular.element(document.body),
-		        targetEvent: $event,
-		        clickOutsideToClose:true,
-		      })
+						$scope.updateUser = function() {
+							Object.toparams = function ObjecttoParams(obj) {
+							    var p = [];
+							    for (var key in obj) {
+							        p.push(key + '=' + encodeURIComponent(obj[key]));
+							    }
+							    return p.join('&');
+							};
+							$http({
+								method:'POST',
+								url:'http://localhost:8000/'+userId+'/update',
+								data:Object.toparams(this.user),
+								headers: {'Content-Type':'application/x-www-form-urlencoded'}
+							}).then(function(response){
+								console.log(response.data);
+								$mdDialog.cancel();
+							})
+						}
+					},
+					templateUrl: 'templates/dialog/edit.html',
+					parent: angular.element(document.body),
+					targetEvent: $event,
+					clickOutsideToClose:true,
+				})
+			}
+        	$scope.createDialog = function($event) {
+	        	$mdDialog.show({
+			        controller: 'UserCreateController',
+			        controllerAs: 'create',
+			        templateUrl: 'templates/dialog/create.html',
+			        parent: angular.element(document.body),
+			        targetEvent: $event,
+			        clickOutsideToClose:true,
+			    })
 
         }
 	}])
 
+	.controller('UserEditController', ['$timeout',
+		'$scope',
+		'$mdDialog',
+		'$http',
+		'$state',
+		function($timeout, $scope, $mdDailog, $http, $state) {
+			$scope.URL = 'http://localhost:8000';
+			// console.log(userId);
+			console.log($state);
+			$scope.cancel = function() {
+				$mdDialog.cancel();
+			}
+
+		}])
+
 	.controller('UserCreateController',['$timeout',
 		'$scope', 
 		'$mdDialog',
-		function($timeout, $scope, $mdDialog) {
+		'$http',
+		function($timeout, $scope, $mdDialog, $http) {
+			$scope.URL = 'http://localhost:8000';
 			$scope.cancel = function() {
 				$mdDialog.cancel();
 			}
 
 			$scope.createUser = function() {
-				console.log(this.user);
-				$mdDialog.hide();
-				Service.getUser();
-			}
+				data = this.user;
+				Object.toparams = function ObjecttoParams(obj) {
+				    var p = [];
+				    for (var key in obj) {
+				        p.push(key + '=' + encodeURIComponent(obj[key]));
+				    }
+				    return p.join('&');
+				};
+				$http({
+					method:'POST',
+					url:$scope.URL+'/store',
+					data:Object.toparams(data),
+					headers: {'Content-Type':'application/x-www-form-urlencoded'}
+				}).then(function(response){
+					console.log(response.data);
+					$mdDialog.cancel();
+				})
+				console.log(data)
+			};
 	}])
